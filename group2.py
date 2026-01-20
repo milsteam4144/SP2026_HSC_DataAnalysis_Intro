c03c8b670dbaeb3ed2033d0e52b7f098336c52a3
rosco-2999 <robinson1589@student.faytechcc.edu>
2026-01-20 18:41:31 +0000

    share

--- group2.py
+++ group2.py
@@ -1,1 +1,20 @@
 # Create a program that creates age categories. Then create a plot or chart that shows how many individuals of each age group survived and did not survive.
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+df = pd.read_csv("Titanic-Dataset.csv")
+
+age = df["Age"]
+survivors = df["Survived"]
+
+data_list = ["age"]
+print(data_list)
+
+if age <= 20 and survivors == 1:
+    print("hi")
+
+    
+
+
+
